0185692424f14429a1324d07c7cab62556fd1e51
JamesDCowley <wuz7@txstate.edu>
2024-07-06 18:24:09 -0500

    tasks can now be added

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function addTask(){
     const taskInput = document.getElementById('task-input');
+    const taskList = document.getElementById('task-list');
     console.log(taskInput.value);
+    
+    const task = document.createElement('div');
+    task.className = 'task text-center';
+    task.innerHTML = `
+        <p>${taskInput.value}</p>
+    `;
+    taskList.appendChild(task);
 }
