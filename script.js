a9aae047793a0f97694f44623f5098ad900ab6db
JamesDCowley <wuz7@txstate.edu>
2024-07-06 21:17:37 -0500

    style changes

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function addTask(){
     const task = document.createElement('div');
     task.className = 'task text-center';
     task.innerHTML = `
-        <p id="task-text" onclick="this.parentElement.remove()" onmouseover="this.style.textDecoration = 'line-through'" onmouseout="this.style.textDecoration = ''">${taskInput.value}</p>
+        <p class="task-text" onclick="this.parentElement.remove()" onmouseover="this.style.textDecoration = 'line-through'" onmouseout="this.style.textDecoration = ''">${taskInput.value}</p>
     `;
     taskList.appendChild(task);
 }
